1f06b89f83038dfa04ba0b91e2828ae6ef50c3ec
Roia2529 <indiroia.ece95@g2.nctu.edu.tw>
2017-09-03 23:45:41 -0600

    initialize hw2

---
+++ hw2/js/Tree.js
@@ -0,0 +1,41 @@
+/** Class representing a Tree. */
+class Tree {
+	/**
+	 * Creates a Tree Object
+	 * parentNode, children, parentName,level,position
+	 * @param {json[]} json - array of json object with name and parent fields
+	 */
+	constructor(json) {
+		
+	}
+
+	/**
+	 * Function that builds a tree from a list of nodes with parent refs
+	 */
+	buildTree() {
+
+	//Assign Positions and Levels by making calls to assignPosition() and assignLevel()
+	}
+
+	/**
+	 * Recursive function that assign positions to each node
+	 */
+	assignPosition(node, position) {
+		
+	}
+
+	/**
+	 * Recursive function that assign levels to each node
+	 */
+	assignLevel(node, level) {
+		
+	}
+
+	/**
+	 * Function that renders the tree
+	 */
+	renderTree() {
+
+	}
+		
+}
